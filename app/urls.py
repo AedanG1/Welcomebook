1c96705480d8502e62841aa013a01e4a455d3905
Aedan Gallivan <120228461+AedanG1@users.noreply.github.com>
2024-08-17 20:36:56 +1000

    Deleted 3 edit paths

--- app/urls.py
+++ app/urls.py
@@ -13,9 +13,6 @@ urlpatterns = [
 
     # API routes
     path("edit_item/<int:item_id>", views.edit_item, name="edit_item"),
-    path("edit_rule/<int:rule_id>", views.edit_rule, name="edit_rule"),
-    path("edit_info/<int:info_id>", views.edit_info, name="edit_info"),
-    path("edit_eats/<int:eats_id>", views.edit_eats, name="edit_eats"),
     path("add_new_item", views.add_new_item, name="add_new_item"),
     path("delete_item/<int:item_id>", views.delete_item, name="delete_item"),
     path("edit_position", views.edit_position, name="edit_position"),
