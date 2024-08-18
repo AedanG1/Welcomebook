708c76a89076fae7ac75f02628fd0f62945fb324
Aedan Gallivan <120228461+AedanG1@users.noreply.github.com>
2024-08-18 17:34:55 +1000

    Removed unused css class selectors for labels

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Rule, Information, Eats, Activity, Contacts
